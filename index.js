3ba869076126eca3ee7902de14554e7599da23bf
davenewham <davidpaulnewham@gmail.com>
2025-11-07 18:13:27 +0100

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/sleep/:duration', (req, res) => {
+  const duration = parseFloat(req.params.duration)
+
+  if (isNaN(duration) || duration > 300) {
+    return res.status(400).send(`Invalid delay request ${duration}. Max is 300 seconds`)
+  }
+  
+  setTimeout(() => {
+    res.send(`Delayed request for ${duration} seconds`)
+  }, duration * 1_000)
+})
+
+app.listen(port, () => {
+  console.log(`Started on ${port}`)
+})
